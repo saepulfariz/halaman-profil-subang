0400209e1c01863ba6fc76d0e600b64e730437f4
saepulfariz <saepulfariz108@gmail.com>
2023-11-23 11:01:58 +0700

    add dropdown menu wisata

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,21 @@ $('.page-scroll').on('click', function (e) {
 
   e.preventDefault();
 })
+
+
+function myDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
